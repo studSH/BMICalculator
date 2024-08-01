727201e17fd0a0c8964e7177c7449ca91373bd73
studSH <s_heinrichs22@stud.hwr-berlin.de>
2024-08-01 18:21:31 +0200

    ich habe allen elementen eine id gegebenen und diese als variable in der main activity java als variable angelegt

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.bmicalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +26,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        TextView resultText = findViewById(R.id.text_view_result);
+
+        RadioButton femaleButton = findViewById(R.id.radio_button_female);
+        RadioButton maleButton = findViewById(R.id.radio_button_male);
+
+        EditText editTextAge = findViewById(R.id.edit_text_age);
+        EditText editTextFeet = findViewById(R.id.edit_text_feet);
+        EditText editTextInches = findViewById(R.id.edit_text_inches);
+        EditText editTextWeight = findViewById(R.id.edit_text_weight);
+
+        Button calculateButton = findViewById(R.id.button_calculate);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "wohooo, ich kann button clicks machen", Toast.LENGTH_LONG).show();            }
+        });
+
     }
 }
