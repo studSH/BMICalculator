f90635f7b64c0aed16d629572c002906d0c95eb1
studSH <s_heinrichs22@stud.hwr-berlin.de>
2024-08-05 16:53:56 +0200

    Fix errors  and update code

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -2,11 +2,11 @@ package com.example.bmicalculator;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,37 +14,98 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultText;
+    private Button calculateButton;
+    private RadioButton femaleButton;
+    private RadioButton maleButton;
+    private EditText editTextAge;
+    private EditText editTextFeet;
+    private EditText editTextInches;
+    private EditText editTextWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        findViews();
+        setupButtonClickListener();
+    }
 
+    private void findViews() {
+        resultText = findViewById(R.id.text_view_result);
+        calculateButton = findViewById(R.id.button_calculate);
+        femaleButton = findViewById(R.id.radio_button_female);
+        maleButton = findViewById(R.id.radio_button_male);
+        editTextAge = findViewById(R.id.edit_text_age);
+        editTextFeet = findViewById(R.id.edit_text_feet);
+        editTextInches = findViewById(R.id.edit_text_inches);
+        editTextWeight = findViewById(R.id.edit_text_weight);
+    }
 
-        TextView resultText = findViewById(R.id.text_view_result);
-
-        RadioButton femaleButton = findViewById(R.id.radio_button_female);
-        RadioButton maleButton = findViewById(R.id.radio_button_male);
-
-        EditText editTextAge = findViewById(R.id.edit_text_age);
-        EditText editTextFeet = findViewById(R.id.edit_text_feet);
-        EditText editTextInches = findViewById(R.id.edit_text_inches);
-        EditText editTextWeight = findViewById(R.id.edit_text_weight);
-
-        Button calculateButton = findViewById(R.id.button_calculate);
-
+    private void setupButtonClickListener() {
         calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "wohooo, ich kann button clicks machen", Toast.LENGTH_LONG).show();            }
-        });
+                double bmiResult = calculateBmi();
+                String ageText = editTextAge.getText().toString();
+                int age = Integer.parseInt(ageText);
 
+                if (age >= 18) {
+                    displayResult(bmiResult);
+                } else {
+                    displayGuidance(bmiResult);
+                }
+            }
+        });
+    }
+
+    private double calculateBmi() {
+        String feetText = editTextFeet.getText().toString();
+        String inchesText = editTextInches.getText().toString();
+        String weightText = editTextWeight.getText().toString();
+
+        int feet = Integer.parseInt(feetText);
+        int inches = Integer.parseInt(inchesText);
+        int weight = Integer.parseInt(weightText);
+
+        int totalInches = (feet * 12) + inches;
+        double heightInMeters = totalInches * 0.0254;
+        return weight / (heightInMeters * heightInMeters);
+    }
+
+    private void displayResult(double bmi) {
+        DecimalFormat myDecimalFormatter = new DecimalFormat("0.00");
+        String bmiTextResult = myDecimalFormatter.format(bmi);
+        String fullResultString;
+
+        if (bmi < 18.5) {
+            fullResultString = bmiTextResult + " - You are underweight";
+        } else if (bmi > 25) {
+            fullResultString = bmiTextResult + " - You are overweight";
+        } else {
+            fullResultString = bmiTextResult + " - You are healthy";
+        }
+
+        resultText.setText(fullResultString);
+    }
+
+    private void displayGuidance(double bmi) {
+        DecimalFormat myDecimalFormatter = new DecimalFormat("0.00");
+        String bmiTextResult = myDecimalFormatter.format(bmi);
+        String fullResultString;
+
+        if (maleButton.isChecked()) {
+            fullResultString = bmiTextResult + " - Wenn du unter 18 bist, spreche bitte mit deinem Arzt für die gesunden Maße bei Jungen";
+        } else if (femaleButton.isChecked()) {
+            fullResultString = bmiTextResult + " - Wenn du unter 18 bist, spreche bitte mit deinem Arzt für die gesunden Maße bei Mädchen";
+        } else {
+            fullResultString = bmiTextResult + " - Wenn du unter 18 bist, spreche bitte mit deinem Arzt für die gesunden Maße";
+        }
+
+        resultText.setText(fullResultString);
     }
 }
